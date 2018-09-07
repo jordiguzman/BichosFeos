fc1ed08f8ad33352e71ba8eea6b95cf01bd54b62
Jordi Guzmán <jordiguz@gmail.com>
2018-09-07 10:12:48 +0200

    Change url videos

--- app/src/main/java/appkite/jordiguzman/com/bichosfeos/Video.java
+++ app/src/main/java/appkite/jordiguzman/com/bichosfeos/Video.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
@@ -15,18 +16,12 @@ import android.widget.VideoView;
 public class Video extends AppCompatActivity {
 
     String url[] = {
-            "https://firebasestorage.googleapis.com/v0/b/friendychat-1e0b0.appspot." +
-                    "com/o/video%2F01_pulga_gato.mp4?alt=media&token=3df4dd53-fefd-4d20-ae7d-8faefc39ff04",
-            "https://firebasestorage.googleapis.com/v0/b/friendychat-1e0b0.appspot." +
-                    "com/o/video%2F02_piojo.mp4?alt=media&token=8a3005dc-833a-4747-97a2-5a8dd27c2487",
-            "https://firebasestorage.googleapis.com/v0/b/friendychat-1e0b0.appspot." +
-                    "com/o/video%2F03_mosca_fruta.mp4?alt=media&token=95cca4bb-ea2e-4528-baaf-cc9ed3ebe13b",
-            "https://firebasestorage.googleapis.com/v0/b/friendychat-1e0b0.appspot." +
-                    "com/o/video%2F04_ojo_mosquito.mp4?alt=media&token=16a45b42-0fda-4dab-bc1f-c2da2b626cb6",
-            "https://firebasestorage.googleapis.com/v0/b/friendychat-1e0b0.appspot." +
-                    "com/o/video%2F05_variadas1.mp4?alt=media&token=741ef166-f998-4fcf-a840-e93e70763501",
-            "https://firebasestorage.googleapis.com/v0/b/friendychat-1e0b0.appspot." +
-                    "com/o/video%2F06_variadas2.mp4?alt=media&token=9face498-5872-4f4d-8af5-aa8f8fe7719e"
+            "https://firebasestorage.googleapis.com/v0/b/xatentresol-87375.appspot.com/o/videos%2F01_pulga_gato.mp4?alt=media&token=a7f51f96-d8a0-4dd3-b4d0-10a0252ef4f1",
+            "https://firebasestorage.googleapis.com/v0/b/xatentresol-87375.appspot.com/o/videos%2F02_piojo.mp4?alt=media&token=c17ef249-144c-4ae9-afd5-bd74629287c2",
+            "https://firebasestorage.googleapis.com/v0/b/xatentresol-87375.appspot.com/o/videos%2F03_mosca_fruta.mp4?alt=media&token=886cc58e-6125-4543-94bc-66fd3d31ae87",
+            "https://firebasestorage.googleapis.com/v0/b/xatentresol-87375.appspot.com/o/videos%2F04_ojo_mosquito.mp4?alt=media&token=8ccb2232-0430-4b2b-9963-573e631841ac",
+            "https://firebasestorage.googleapis.com/v0/b/xatentresol-87375.appspot.com/o/videos%2F05_variadas1.mp4?alt=media&token=d2d5446b-a9cb-4921-a52f-3933562d9797",
+            "https://firebasestorage.googleapis.com/v0/b/xatentresol-87375.appspot.com/o/videos%2F06_variadas2.mp4?alt=media&token=e120500b-6c0b-40bb-a8f3-0cbca3de21fb"
     };
 
     VideoView videoView;
@@ -82,26 +77,32 @@ public class Video extends AppCompatActivity {
     public void repVideo() {
 
 
+        try{
+            mediaController = new MediaController(this);
+            mediaController.setAnchorView(videoView);
 
-        uriPath = url[MenuVideo.video];
-        uri2 = Uri.parse(uriPath);
-        videoView.setVideoURI(uri2);
-        videoView.requestFocus();
 
-        mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
-        videoView.start();
-        progressBar.setVisibility(View.VISIBLE);
+            uriPath = url[MenuVideo.video];
+            uri2 = Uri.parse(uriPath);
+            videoView.setVideoURI(uri2);
+            videoView.requestFocus();
+
+
+            videoView.setMediaController(mediaController);
+            progressBar.setVisibility(View.VISIBLE);
+
+        }catch (Exception e){
+            Log.d("Error", e.getCause().toString());
+        }
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                mp.start();
+               videoView.start();
                 mp.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
                     @Override
                     public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
                         progressBar.setVisibility(View.GONE);
-                        mp.start();
+                        videoView.start();
                     }
                 });
             }
